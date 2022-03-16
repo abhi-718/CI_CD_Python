c7ecff31602ac616d1bade0a9fb417af3c6cbd3a
ABHISEK MISHRA <mishra08051997@gmail.com>
2022-03-16 14:13:49 +0530

    Calculator Version 1 added

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest import result
+from src import app
+
+class Testcalc(unittest.TestCase):
+
+    def test_add(self):
+        result = app.add(10,15)
+        self.assertEqual(result,25)
+
+    def test_sub(self):
+        result = app.sub(10,5)
+        self.assertEqual(result,5)
+
+    def test_mul(self):
+        result = app.mul(4,3)
+        self.assertEqual(result,12)
+    
+        
+
+
